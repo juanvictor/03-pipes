73fe3a6afb89db000bf27b5837ca4931fdacdfdc
JUAN VICTOR SERRUDO CHAVEZ <juan.vsc@gmail.com>
2020-03-06 17:40:44 -0400

    20200306 Primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipes';
+
+    nombre: string = 'Capitán América';
+    arreglo = [1,2,3,4,5,6,7,8,9,10];
+
 }
