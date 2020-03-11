58b287bcc53b1e4b63259bf88eb8a9de6ae367e4
JUAN VICTOR SERRUDO CHAVEZ <juan.vsc@gmail.com>
2020-03-11 19:43:25 -0400

    20200311 PIPES ejemplos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,33 @@ import { Component } from '@angular/core';
 export class AppComponent {
 
     nombre: string = 'Capitán América';
+    nombre2: string = 'juAn vICToR SERrudO';
     arreglo = [1,2,3,4,5,6,7,8,9,10];
+    PI: number = Math.PI;
+    porcentaje: number = 0.234;
+    salario: number = 1234.5;
+    heroe = {
+        nombre: 'Logan',
+        clave: 'Wolverine',
+        edad: 500,
+        direccion: {
+            calle: 'Primera',
+            casa: 20
+        }
+    }
+
+    valorPromesa = new Promise<string>( (resolve) => {
+
+        setTimeout(() => {
+            resolve('llego la data');
+        }, 4500);
+
+    });
+
+    fecha: Date = new Date();
+
+    idioma: string = 'en';
+
+    videoUrl: string ='https://www.youtube.com/embed/jB3FMQe0J4w';
 
 }
